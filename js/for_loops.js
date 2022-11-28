9ad2080f9dfd3aeae75fbed2bd4873b07debda34
Songju Oh <songju.oh1@gmail.com>
2022-11-28 09:08:04 -0600

    fixed some codes for loop

--- js/for_loops.js
+++ js/for_loops.js
@@ -12,3 +12,57 @@ showMultiplicationTable(7);
 //     console.log('for loop iteration #' + i + ', j = ' + j);
 // }
 
+
+// let randomNumber = number[Math.floor(Math.random() * colors.length)];
+// var luckyNumber = Math.floor(Math.random() * 6);
+
+
+//Version 1: basic for loop with a condition
+for(var i =0; i<3; i++){
+    var randomNumber = Math.floor(Math.random()*200)+20;
+    if (randomNumber % 2 === 0) {
+        console.log(randomNumber + " is even");
+    } else if (randomNumber % 2 === 1) {
+        console.log(randomNumber + " is odd");
+    }
+}
+
+//Version 2: used function for input
+function randomEvenOdd (min, max){
+    for(var i = 0; i<3; i++) {
+
+        var randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+        if (randomNumber % 2 === 0) {
+            console.log(randomNumber + " is even");
+        } else if (randomNumber % 2 === 1) {
+            console.log(randomNumber + " is odd");
+        }
+    }
+}
+randomEvenOdd(200, 20);
+
+
+for(var i = 1; i<10; i++){
+    let number = '';
+    for(var j = 0; j<i; j++){
+        number += i;
+    }
+    console.log(number);
+}
+// Version 1: used for loop only, but hard coded
+for(var i = 0; i<20; i++){
+    console.log(100-(i*5));
+}
+
+// Version 2: used function for input
+function minusFive (number){
+    let outcome ='';
+    for (var i = 0; i<number; i++){
+        outcome = number - (i*5);
+        if(outcome === 0){
+            break;
+        }
+    console.log(outcome);
+    }
+}
+minusFive(50);
