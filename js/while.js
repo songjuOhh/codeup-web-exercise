3ee8cdfc19e88f75e374c8f0fb6e0ee281707cdf
Songju Oh <songju.oh1@gmail.com>
2022-11-29 14:08:00 -0600

    fixed some codes for loop

--- js/while.js
+++ js/while.js
@@ -1,3 +1,6 @@
+//While Loops #2
+console.log("While Loops #2");
+console.log("");
 var i = 1;
 
 while (i <17){
@@ -7,7 +10,12 @@ while (i <17){
     i++;
 }
 
-function canGoHome(cones){
+
+//Do
+console.log("");
+console.log("Do While Loops #1");
+console.log("");
+function letsSellCones(cones){
     let randomCones = Math.floor(Math.random()*50)+50;
     let allCones = parseFloat(randomCones);
     console.log ("You have "+allCones+" cones today.");
@@ -22,18 +30,23 @@ function canGoHome(cones){
             remains = remains - sellingCones;
 
             // cone or cones
-            if(sellingCones <= 1 || remains <=1){
+            if(sellingCones <= 1) {
                 console.log(sellingCones + " cone sold...");
-                console.log("       ("+remains+ " cone left)");
-            }else {
-                console.log(sellingCones + " cones sold...");
-                console.log("       ("+remains + " cones left)");
-            }
+            } else  console.log(sellingCones + " cones sold...");
+            if (remains <=1) {
+                console.log("       (" + remains + " cone left)");
+            } else console.log("       ("+remains + " cones left)");
+
             console.log("");
 
         }else if(sellingCones > remains && remains !==0) {
-            console.log("Cannot sell you " + sellingCones +" cones. I only have "+remains);
-            console.log("")
+
+            // cone vs cones
+            if (remains ===1){
+                console.log("Cannot sell you " + sellingCones +" cone. I only have "+remains);
+            } else {
+                console.log("Cannot sell you " + sellingCones + " cones. I only have " + remains);
+            } console.log("")
 
         } else if( remains === 0){
             console.log("Yay! I sold them all!");
@@ -42,6 +55,7 @@ function canGoHome(cones){
 
     } while (i<50);
 }
-canGoHome("");
+
+letsSellCones("");
 
 
